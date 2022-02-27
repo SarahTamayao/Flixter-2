13cd0ebb2bc382f810578af4c12a8a06191b3197
Ahmad Hossain <ahmad.awaab@gmail.com>
2022-02-26 23:26:28 -0500

    - Movie trailer is only played if movie is popular - Minor styling in activity_detail

--- app/src/main/java/com/example/flixter/DetailActivity.kt
+++ app/src/main/java/com/example/flixter/DetailActivity.kt
@@ -2,8 +2,6 @@ package com.example.flixter
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.RatingBar
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
@@ -11,7 +9,6 @@ import com.example.flixter.databinding.ActivityDetailBinding
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
-import com.google.android.youtube.player.YouTubePlayerView
 import okhttp3.Headers
 
 private const val TRAILERS_URL =
@@ -30,6 +27,9 @@ class DetailActivity : YouTubeBaseActivity() {
         //title, overview, and rating are set using data binding
         binding.movie = movie
 
+        //todo if movie rating is 5+ stars, play video immediately
+        //     else show an image preview that can initiate playing a YT vid
+
         val client = AsyncHttpClient()
         client.get(TRAILERS_URL.format(movie.movieId), object : JsonHttpResponseHandler() {
             override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
@@ -42,7 +42,7 @@ class DetailActivity : YouTubeBaseActivity() {
                 val movieTrailerJson = results.getJSONObject(0)
                 val youtubeKey = movieTrailerJson.getString("key")
                 //play yt video with this trailer
-                initializeYoutube(youtubeKey)
+                initializeYoutube(youtubeKey, movie.voteAverage)
             }
 
             override fun onFailure(
@@ -51,26 +51,32 @@ class DetailActivity : YouTubeBaseActivity() {
                 Log.i(TAG, "onFailure")
             }
         })
-
     }
 
-    private fun initializeYoutube(youtubeKey: String) {
-        binding.player.initialize(resources.getString(R.string.youtube_api_key), object : YouTubePlayer.OnInitializedListener {
-            override fun onInitializationSuccess(
-                provider: YouTubePlayer.Provider?,
-                player: YouTubePlayer?,
-                p2: Boolean
-            ) {
-                Log.i(TAG, "onInitializationSuccess")
-                player?.loadVideo(youtubeKey)
-            }
+    private fun initializeYoutube(youtubeKey: String, voteAverage: Float) {
+        binding.player.initialize(
+            resources.getString(R.string.youtube_api_key),
+            object : YouTubePlayer.OnInitializedListener {
+                override fun onInitializationSuccess(
+                    provider: YouTubePlayer.Provider?,
+                    player: YouTubePlayer?,
+                    p2: Boolean
+                ) {
+                    Log.i(TAG, "onInitializationSuccess")
+                    //if movie is popular, play vid immediately
+                    if (voteAverage > 5) {
+                        player?.loadVideo(youtubeKey)
+                    } else {
+                        player?.cueVideo(youtubeKey)
+                    }
+                }
 
-            override fun onInitializationFailure(
-                p0: YouTubePlayer.Provider?,
-                p1: YouTubeInitializationResult?
-            ) {
-                Log.i(TAG, "onInitializationFailure: $p1 \n $p0")
-            }
-        })
+                override fun onInitializationFailure(
+                    p0: YouTubePlayer.Provider?,
+                    p1: YouTubeInitializationResult?
+                ) {
+                    Log.i(TAG, "onInitializationFailure: $p1 \n $p0")
+                }
+            })
     }
 }
